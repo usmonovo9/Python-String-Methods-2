e83fb9e689da18ca2717814a5a1b9deb41a67b72
usmonovo9 <usmonovo9@icloud.com>
2025-12-25 21:15:06 +0500

    bajarildi

---
+++ task09.py
@@ -0,0 +1,7 @@
+text = input("Matn kiriting: ")
+searching = input("So'z kiriting: ")
+anything = int(input("Raqam kiriting: "))
+
+result = text.find(searching , anything)
+
+print("Natija: " ,result)
